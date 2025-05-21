d2f3a636efd3f67b7d784102ad23da1848e4f3e0
Jasperb3 <141338069+Jasperb3@users.noreply.github.com>
2025-05-21 20:39:34 +0000

    Update ALLOWED_HOSTS and modify api_root URLs for local and codespace environments

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!9$sv8hcsxyi)18diqyrs=!j@34&)=krlsro@&ma&wsl@7u*m-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'psychic-adventure-6944rv56rp5f9j9-8000.app.github.dev']
 
 
 # Application definition
